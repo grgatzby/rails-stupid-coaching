5a379109a0fd83628266fdeed6962bc01a01ebaf
Guillaume Cazals <gcazals06@gmail.com>
2023-02-20 15:11:52 +0100

    stupid coaching

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class QuestionsTest < ApplicationSystemTestCase
   test "visiting /ask renders the form" do
-    visit ask_url
+    visit "/questions" #ask_url
     assert_selector "p", text: "Ask your coach anything"
   end
 end
